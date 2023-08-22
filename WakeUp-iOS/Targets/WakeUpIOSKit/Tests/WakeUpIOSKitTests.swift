8aa6e9182cdc165346f7437e23deca029a0e2d09
이준영 <junlight94@gmail.com>
2023-08-22 23:40:14 +0900

    create tuist

---
+++ WakeUp-iOS/Targets/WakeUpIOSKit/Tests/WakeUpIOSKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class WakeUpIOSKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("WakeUpIOSKit", "WakeUpIOSKit")
+    }
+}
---
+++ WakeUp-iOS/Targets/WakeUpIOSUI/Tests/WakeUpIOSUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class WakeUpIOSUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("WakeUpIOSUI", "WakeUpIOSUI")
+    }
+}
